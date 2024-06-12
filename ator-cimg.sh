dd4932260d26463aa094e70b0ad442e3f3f5d41b
sic <31908995+sicXnull@users.noreply.github.com>
2024-06-12 18:55:50 -0400

    Update ator-cimg.sh

--- ator-cimg.sh
+++ ator-cimg.sh
@@ -30,38 +30,49 @@ if [[ -z $NICKNAME || -z $EMAIL || -z $ETH_ADDRESS ]]; then
     echo "Usage: $0 --nickname=<nickname> --email=<email> --eth-address=<eth-address>"
     exit 1
 fi
+
+if [ ! -x /usr/bin/docker-compose ]; then
+    echo "docker-compose not found. Installing..."
+    # Download docker-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-$(uname -s)-$(uname -m) -o /usr/bin/docker-compose
+    # Set ownership to root
+    sudo chown root: /usr/bin/docker-compose
+    # Make it executable
+    sudo chmod +x /usr/bin/docker-compose
+    echo "docker-compose installed successfully."
+else
+    echo "docker-compose is already installed."
+fi
+
 # Create directories
-sudo mkdir -p /opt/anon/
-sudo mkdir -p /opt/anon/etc/anon/
-sudo mkdir -p /opt/anon/run/anon/
-sudo mkdir -p /root/.nyx/
+
+mkdir -p /data/anon/
+mkdir -p /data/anon/etc/anon/
+mkdir -p /data/anon/run/anon/
+mkdir -p /data/.nyx/
 
 # Create log file
-sudo touch /opt/anon/etc/anon/notices.log
+touch /data/anon/etc/anon/notices.log
 
 # Set permissions
-sudo chmod -R 700 /opt/anon/run/anon/
-sudo chown -R 100:101 /opt/anon/run/anon/
-sudo chown 100:101 /opt/anon/etc/anon/notices.log
-
-# Add user
-sudo useradd -M anond
+chmod -R 700 /data/anon/run/anon/
+chown -R 100:101 /data/anon/run/anon/
+chmod -R 777 /data/anon/etc/anon/notices.log
 
 # Download configuration files
-sudo wget -O /opt/anon/relay.yaml https://raw.githubusercontent.com/ATOR-Development/anon-install/main/docker/anon-relay/relay.yaml
-sudo wget -O /opt/anon/etc/anon/anonrc https://raw.githubusercontent.com/ATOR-Development/anon-install/main/docker/anon-relay/anonrc
-sudo wget -O /root/.nyx/config https://raw.githubusercontent.com/ATOR-Development/anon-install/main/docker/anon-relay/config
+wget -O /data/anon/relay.yaml https://raw.githubusercontent.com/sicXnull/anon-install/main/docker/anon-relay/relay.yaml
+wget -O /data/anon/etc/anon/anonrc https://raw.githubusercontent.com/sicXnull/anon-install/main/docker/anon-relay/anonrc
+wget -O /root/.nyx/config https://raw.githubusercontent.com/sicXnull/anon-install/main/docker/anon-relay/config
 
 # Create the content for the anonrc file
-cat <<EOF > /opt/anon/etc/anon/anonrc
-User anond
+cat <<EOF > /data/anon/etc/anon/anonrc
+User anond 
 DataDirectory /var/lib/anon
 ControlSocket /run/anon/control
 ControlSocketsGroupWritable 1
 CookieAuthentication 1
 CookieAuthFile /run/anon/control.authcookie
 CookieAuthFileGroupReadable 1
-Log notice file /etc/anon/notices.log
 ORPort 9001
 ExitRelay 0
 Nickname $NICKNAME
@@ -69,5 +80,11 @@ ContactInfo $EMAIL @anon:$ETH_ADDRESS
 EOF
 
 echo "anonrc file created successfully."
+
+# clear old containers
+
+docker stop anon-relay
+docker rm anon-relay
+
 # Start Docker container
-sudo docker-compose -f /opt/anon/relay.yaml up -d
+docker-compose -f /data/anon/relay.yaml up -d
