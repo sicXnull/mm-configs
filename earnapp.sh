f08981141e82f66ae49390995e752557b1dfb4bf
sicXnull <cvanhauterjr@gmail.com>
2024-08-15 02:13:52 +0100

    update script

--- earnapp.sh
+++ earnapp.sh
@@ -4,11 +4,22 @@ generate_md5sum() {
     echo -n "$1" | md5sum | awk '{ print $1 }'
 }
 
-RANDOM_STRING=$(date +%s | sha256sum | base64 | head -c 32)
-MD5SUM=$(generate_md5sum "$RANDOM_STRING")
-EARNAPP_DEVICE_ID="sdk-node-$MD5SUM"
+UUID_FILE="EARNAPP_DEVICE_ID"
 
-echo "Generated EARNAPP_DEVICE_ID: $EARNAPP_DEVICE_ID"
+# Check if the UUID file exists
+if [[ -f "$UUID_FILE" ]]; then
+    # Read the UUID from the file
+    EARNAPP_DEVICE_ID=$(cat "$UUID_FILE")
+    echo "Using existing EARNAPP_DEVICE_ID: $EARNAPP_DEVICE_ID"
+else
+    # Generate a new UUID and save it to the file
+    RANDOM_STRING=$(date +%s | sha256sum | base64 | head -c 32)
+    MD5SUM=$(generate_md5sum "$RANDOM_STRING")
+    EARNAPP_DEVICE_ID="sdk-node-$MD5SUM"
+    
+    echo "Generated EARNAPP_DEVICE_ID: $EARNAPP_DEVICE_ID"
+    echo "$EARNAPP_DEVICE_ID" > "$UUID_FILE"
+fi
 
 docker run -d --name earnapp \
     --restart always \
